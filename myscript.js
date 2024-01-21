8a223433dbe91b342c32dda7bd08e61de63fa16a
Daniel <s10258472@connect.np.edu.sg>
2024-01-21 20:58:52 +0800

    started on the women page

---
+++ myscript.js
@@ -0,0 +1,24 @@
+includeHTML();
+function includeHTML() {
+  var z, i, a, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    if (z[i].getAttribute("myscript")) {
+      a = z[i].cloneNode(false);
+      file = z[i].getAttribute("myscript");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          a.removeAttribute("myscript");
+          a.innerHTML = xhttp.responseText;
+          z[i].parentNode.replaceChild(a, z[i]);
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+  
