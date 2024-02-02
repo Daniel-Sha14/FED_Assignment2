558664411fc4307557cd107079f618f7bad33182
Daniel <s10258472@connect.np.edu.sg>
2024-02-02 12:24:01 +0800

    implemented search function and put links in footer

--- myscript.js
+++ myscript.js
@@ -21,4 +21,72 @@ function includeHTML() {
     }
   }
 }
+
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    searchItems();
+  }
+}
+
+function searchItems() {
+  // Get the search input value
+  var searchTerm = document.getElementById('search-bar').value.toLowerCase();
+
+  // Check for keywords related to items and redirect accordingly
+  if (searchTerm.includes('women') && searchTerm.includes('accessories')) {
+    window.location.href = 'waccessories.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('bottoms')) {
+    window.location.href = 'wbottoms.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('dresses')) {
+    window.location.href = 'wdressesandskirts.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('skirts')) {
+    window.location.href = 'wdressesandskirts.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('innerwear')) {
+    window.location.href = 'winnerwear.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('homewear')) {
+    window.location.href = 'whomewear.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('maternity')) {
+    window.location.href = 'maternity.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('outerwear')) {
+    window.location.href = 'wouterwear.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('sportswear')) {
+    window.location.href = 'wsportswear.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('tops')) {
+    window.location.href = 'wtops.html';
+  } else if (searchTerm.includes('women') && searchTerm.includes('underwear')) {
+    window.location.href = 'wunderwear.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('accessories')) {
+    window.location.href = 'maccessories.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('bottoms')) {
+    window.location.href = 'mbottoms.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('homewear')) {
+    window.location.href = 'mhomewear.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('innerwear')) {
+    window.location.href = 'minnerwear.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('outerwear')) {
+    window.location.href = 'mouterwear.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('sportswear')) {
+    window.location.href = 'msportswear.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('tops')) {
+    window.location.href = 'mtops.html';
+  } else if (searchTerm.includes('men') && searchTerm.includes('underwear')) {
+    window.location.href = 'munderwear.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('homewear')) {
+    window.location.href = 'khomewear.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('innerwear')) {
+    window.location.href = 'kinnerwear.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('outerwear')) {
+    window.location.href = 'kouterwear.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('tops')) {
+    window.location.href = 'ktops.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('sportswear')) {
+    window.location.href = 'ksportswear.html';
+  } else if (searchTerm.includes('kids') && searchTerm.includes('underwear')) {
+    window.location.href = 'kunderwear.html';
+  } 
+  else {
+    alert('Item not found!');
+  }
+}
+
   
